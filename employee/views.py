8563273f85d90d6f09bb74c09482a013c37eb39e
asif-bhai-xoss <78955762+asif-bhai-xoss@users.noreply.github.com>
2021-02-12 16:30:03 +0600

    Update views.py

--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def employee(request):
+def home_view(request):
     return render(request, "employee/index.html")
+
+
+def contact_view(request):
+    return render(request, "employee/contact.html")
